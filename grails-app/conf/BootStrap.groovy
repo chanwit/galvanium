1d35c9570b6a9e6a67259a0484653ad6473f6d70
Chanwit Kaewkasi <chanwit@gmail.com>
2010-10-02 21:58:33 +0700

    added more module/tabs.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,8 +8,15 @@ class BootStrap {
         // Setting up some modules
         //
         new Module(name:"Overview").save()
+        new Module(name:"Download", enabled:"false").save()
+        new Module(name:"Activity").save()
+        new Module(name:"Roadmap").save()
+        new Module(name:"Issues").save()
+        new Module(name:"News", enabled:"false").save()
+        new Module(name:"Wiki", enabled:"false").save()
+        new Module(name:"Forums", enabled:"false").save()
         new Module(name:"Repository", url:"module/git.zul").save()
-        
+
         //
         // Setting up some configs
         //
