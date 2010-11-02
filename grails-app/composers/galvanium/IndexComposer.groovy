f721682257d5c2259c1b0a85ba9916cf44325fe0
Chanwit Kaewkasi <chanwit@gmail.com>
2010-11-02 15:48:58 +0700

    drafting MVP-like controls.

--- grails-app/composers/galvanium/IndexComposer.groovy
+++ grails-app/composers/galvanium/IndexComposer.groovy
@@ -3,9 +3,20 @@ package galvanium
 import org.zkoss.zkgrails.*
 import org.zkoss.zk.ui.event.*
 
+//
+// Shell composer
+//
 class IndexComposer extends GrailsComposer {
 
+
     def mainInc
+
+    def places = {
+        "/$activity/$id?" {
+           container = mainInc 
+        }
+    }
+
 
     def onBookmarkChange_wndMain(BookmarkEvent ev) {
         mainInc.src = "core/${ev.bookmark}.zul"
